b03e5c556214f623e4065016784652f1a01ca8d0
otaki0413 <67969654+otaki0413@users.noreply.github.com>
2022-10-04 23:15:47 +0900

    Organismの作成_1

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img src="" alt="プロフィール" />
+      <p>名前</p>　
+      <dl>
+        <dt>メール</dt>
+        <dd>test@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-222-555</dd>
+        <dt>会社名</dt>
+        <dd>株式会社テスト</dd>
+        <dt>WEB</dt>
+        <dd>testtest.com</dd>
+      </dl>
+    </div>
+  );
+};
