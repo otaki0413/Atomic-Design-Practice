80552918c08b366340ba32fe25b9d55ed470cda2
otaki0413 <67969654+otaki0413@users.noreply.github.com>
2022-10-08 13:34:29 +0900

    Pageの作成

--- src/components/templates/DefaultLayout.jsx
+++ src/components/templates/DefaultLayout.jsx
@@ -3,6 +3,7 @@ import { Header } from '../atoms/layout/Header';
 
 export const DefaultLayout = (props) => {
   const { children } = props;
+  console.log(props);
   return (
     <>
       <Header />
